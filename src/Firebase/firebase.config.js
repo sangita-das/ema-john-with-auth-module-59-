f4ee66ffa4b9fa70e0aae2d6df002bf0ac0b719c
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-10-12 09:04:49 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1WiqkkWADta7UEjZNa1aRyi_5z_1Q6O4",
+  authDomain: "ema-john-simple-45078.firebaseapp.com",
+  projectId: "ema-john-simple-45078",
+  storageBucket: "ema-john-simple-45078.appspot.com",
+  messagingSenderId: "778065620319",
+  appId: "1:778065620319:web:20b46d99b8bbb8ae261aea"
+};
+
+export default firebaseConfig;
